69e2de97583eb1739cd706652e4894d791a7d1d2
Anvar Alimov <87275043+anvaralimov@users.noreply.github.com>
2022-07-08 11:59:32 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,10 @@ clear
 sudo rm -rf /etc/pacman.d/mirrorlist
 sudo cp -r ./mirrorlist /etc/pacman.d/
 
-sudo pacman -Syyu
-
-sudo pacman -S figlet lolcat
+sudo pacman -Syy figlet lolcat
 
 clear
 
 echo "Tayyor, O'zbekiston mirrori o'rnatildi..."
 
-filet "Xinux | Uzinfocom 2022" | lolcat
+figlet  -c "Xinux | Uzinfocom 2022" | lolcat
